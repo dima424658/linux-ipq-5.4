92d94d43f743eda171ad271fab2b96a2a13bac49
Linus Walleij <linus.walleij@linaro.org>
2024-05-01 21:46:31 +0200

    net: ethernet: cortina: Restore TSO support
    An earlier commit deleted the TSO support in the Cortina Gemini
    driver because the driver was confusing gso_size and MTU,
    probably because what the Linux kernel calls "gso_size" was
    called "MTU" in the datasheet.
    
    Restore the functionality properly reading the gso_size from
    the skbuff.
    
    Tested with iperf3, running a server on a different machine
    and client on the device with the cortina gemini ethernet:
    
    Connecting to host 192.168.1.2, port 5201
    60008000.ethernet-port eth0: segment offloading mss = 05a8 len=1c8a
    60008000.ethernet-port eth0: segment offloading mss = 05a8 len=1c8a
    60008000.ethernet-port eth0: segment offloading mss = 05a8 len=27da
    60008000.ethernet-port eth0: segment offloading mss = 05a8 len=0b92
    60008000.ethernet-port eth0: segment offloading mss = 05a8 len=2bda
    (...)
    
    It also performs well: ~268 MBit/s.
    
    Fixes: ac631873c9e7 ("net: ethernet: cortina: Drop TSO support")
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/net/ethernet/cortina/gemini.c
+++ drivers/net/ethernet/cortina/gemini.c
@@ -79,7 +79,8 @@ MODULE_PARM_DESC(debug, "Debug level (0=none,...,16=all)");
 #define GMAC0_IRQ4_8 (GMAC0_MIB_INT_BIT | GMAC0_RX_OVERRUN_INT_BIT)
 
 #define GMAC_OFFLOAD_FEATURES (NETIF_F_SG | NETIF_F_IP_CSUM | \
-			       NETIF_F_IPV6_CSUM | NETIF_F_RXCSUM)
+			       NETIF_F_IPV6_CSUM | NETIF_F_RXCSUM | \
+			       NETIF_F_TSO | NETIF_F_TSO_ECN | NETIF_F_TSO6)
 
 /**
  * struct gmac_queue_page - page buffer per-page info
@@ -1148,13 +1149,29 @@ static int gmac_map_tx_bufs(struct net_device *netdev, struct sk_buff *skb,
 	skb_frag_t *skb_frag;
 	dma_addr_t mapping;
 	void *buffer;
+	u16 mss;
 	int ret;
 
-	/* TODO: implement proper TSO using MTU in word3 */
 	word1 = skb->len;
 	word3 = SOF_BIT;
 
-	if (skb->len >= ETH_FRAME_LEN) {
+	mss = skb_shinfo(skb)->gso_size;
+ 	if (mss) {
+		/* skb->len will be all segments in this case */
+		netdev_dbg(netdev, "segment offloading mss = %04x len=%04x\n",
+			   mss, skb->len);
+		word1 |= TSS_MTU_ENABLE_BIT;
+		word3 |= mss;
+	} else {
+		mss = skb->len;
+	}
+
+	/* Translate to link layer size */
+	mss += ETH_HLEN;
+	if (skb->protocol == htons(ETH_P_8021Q))
+		mss += VLAN_HLEN;
+
+	if (mss >= ETH_FRAME_LEN) {
 		/* Hardware offloaded checksumming isn't working on frames
 		 * bigger than 1514 bytes. A hypothesis about this is that the
 		 * checksum buffer is only 1518 bytes, so when the frames get
@@ -1169,7 +1186,9 @@ static int gmac_map_tx_bufs(struct net_device *netdev, struct sk_buff *skb,
 				return ret;
 		}
 		word1 |= TSS_BYPASS_BIT;
-	} else if (skb->ip_summed == CHECKSUM_PARTIAL) {
+	}
+
+	if (skb->ip_summed == CHECKSUM_PARTIAL) {
 		int tcp = 0;
 
 		/* We do not switch off the checksumming on non TCP/UDP
